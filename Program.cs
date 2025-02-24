b2612930b530d6063a722d76bcef0b8a0fb4e9e0
Bouric Enos <bouricenos0@gmail.com>
2025-02-24 16:19:05 +0300

    Welcome to c# programming

--- Program.cs
+++ Program.cs
@@ -1,2 +1,14 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+using System;
+
+class Name
+{
+    static void Main(string [] args)
+    {
+       string name;
+       Console.WriteLine("Enter your Name: ");
+       name = Console.ReadLine();
+       Console.WriteLine("Hello" + name + "welcome to C# programming.");
+    }
+}
